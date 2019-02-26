50360445aaff0b71933126a0827c1e00db6ccbe9
Samuel Guo <guosamuel1114@gmail.com>
2019-02-26 01:54:01 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
 # Write your code here.
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array_of_names)
+  badge_messages = []
+  array_of_names.each do |each_name|
+    badge_messages.push(badge_maker(each_name))
+  end
+  return badge_messages
+end
+
+def assign_rooms(array_of_names)
+  room_assignments = []
+  array_of_names.each_with_index { |item, index|
+    room_assignments.push("Hello, #{item}! You'll be assigned to room #{index+1}!"
+  }    
+end
+
+def printer
+  
+end
