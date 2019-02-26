8fc60c8afe5f96a5b00d825b890af4d32ca97545
Samuel Guo <guosamuel1114@gmail.com>
2019-02-26 02:16:17 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,5 +21,5 @@ def assign_rooms(array_of_names)
 end
 
 def printer(array_of_names)
-  puts batch_badge_creator(array_of_names).concat(assign_room(array_of_names))
+  puts batch_badge_creator(array_of_names).concat(assign_rooms(array_of_names))
 end
